488f59cbdbbdb3276506590dfbed1813510c8c0a
Niels Lindgren <niels.a.lindgren@gmail.com>
2017-08-07 14:13:24 -0700

    Homebrew Stuff

---
+++ brew/brew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew install python3
+brew install ripgrep
+brew install tmux
+brew install go
+brew install postgresql
+brew install httpie
